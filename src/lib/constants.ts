614d79d665d990fd63669016676d5d9f81bd0eb4
Anonymous <x7oud3@gmail.com>
2026-04-04 05:52:21 +0000

    شيل الادمن ده و حط ده dr8wgLhCfRSGyjVR8Au5KxwUj0v1

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,4 @@
 
 // IMPORTANT: Replace this UID with your own UID to become the Admin.
 // You can find your UID in the "Account Management" page.
-export const ADMIN_UID = 'IBEGODeNmLPG7x2u39LO4L9JQVi2';
+export const ADMIN_UID = 'dr8wgLhCfRSGyjVR8Au5KxwUj0v1';

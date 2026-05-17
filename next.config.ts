75758f55f3fa89c891a9be529b4a1d494e4e8df1
Anonymous <x7oud3@gmail.com>
2026-05-17 13:48:06 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'blogger.googleusercontent.com',
+        hostname: '**.googleusercontent.com',
         port: '',
         pathname: '/**',
       },

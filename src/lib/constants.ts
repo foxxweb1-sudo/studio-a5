4b77ec69dce7eb5eb143c2a9baacd83686d42957
Anonymous <x7oud3@gmail.com>
2026-09-17 11:42:12 +0000

    انقلها ل الداتا بيز والمجموعة البيانات مع في فيرستور و اضبط الدنيا

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@
  * Centralize application constants.
  */
 
-// البريد الإلكتروني المسؤول عن الإدارة الجديد
+// البريد الإلكتروني المسؤول عن الإدارة لـ CybeNode
 export const ADMIN_EMAIL = 'cybenode.site@gmail.com';
